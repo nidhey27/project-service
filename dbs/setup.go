7a0d2a625f9738294a13848949deb6352fd6e7a9
nidhey27 <nidhey.indurkar@infracloud.io>
2023-03-04 10:15:13 +0530

    /home/nidhey

---
+++ dbs/setup.go
@@ -0,0 +1,83 @@
+package dbs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/glebarez/sqlite"
+	"github.com/nidhey27/project-service/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var dbInstance *gorm.DB
+
+func GetDB() *gorm.DB {
+	return dbInstance
+}
+
+func setupPostgres() (*gorm.DB, error) {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_DATABASE")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+		dbHost,
+		dbPort,
+		dbUser,
+		dbName,
+		dbPassword)
+
+	db, err := gorm.Open(postgres.Open(connectionString))
+
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func setupSQLite() (*gorm.DB, error) {
+	dbLocation := os.Getenv("DATABASE_PATH")
+	if dbLocation == "" {
+		dbLocation = "/opt/auth-service/gorm.db"
+	}
+
+	// Create the sqlite file if it's not available
+	if _, err := os.Stat(dbLocation); err != nil {
+		if _, err = os.Create(dbLocation); err != nil {
+			return nil, err
+		}
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbLocation), &gorm.Config{})
+	return db, err
+}
+
+func InitializeDatabaseLayer() error {
+	dbs := os.Getenv("DB")
+	var db *gorm.DB
+	var err error
+
+	switch dbs {
+	case "sqlite":
+		db, err = setupSQLite()
+	case "postgres":
+		db, err = setupPostgres()
+	default:
+		return fmt.Errorf("no database found, set the DB env")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	err = models.AutoMigrate(db)
+
+	if err != nil {
+		return err
+	}
+	dbInstance = db
+	return nil
+}
